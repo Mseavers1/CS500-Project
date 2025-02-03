fd69717e9b4019960dd2f8e45d82e0407584d29e
Mseavers1 <91432334+Mseavers1@users.noreply.github.com>
2025-02-03 09:31:02 -0600

    t

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,9 +2,37 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const header = () => {
+        return (
+            <div className="fixed flex top-0 left-0 right-0 p-3 w-full bg-primary_bars">
+
+            </div>
+        )
+    }
+
+    const footer = () => {
+        return (
+            <div className="fixed flex bottom-0 left-0 right-0 p-3 w-full bg-primary_bars">
+
+            </div>
+        )
+    }
+
     return (
-        <div className="bg-amber-600">
-            Hello
+        <div className="h-screen">
+
+            {/** Header **/}
+            {header()}
+
+            {/** Body **/}
+            <div className="flex justify-center items-center h-full bg-background">
+
+            </div>
+
+            {/** Footer **/}
+            {footer()}
+
         </div>
     )
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary_bars: "#1E3A8A",
+        secondary_bars: "#3B82F6",
+        accent: "#E63946",
+        highlight: "#FACC15",
+        background: "#F8FAFC"
+      }
+    },
   },
   plugins: [],
 };
